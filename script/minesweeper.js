e2270dc9d566ccc01e5fb1cd8ffd135cbd141d1b
Jason107206 <53583996+Jason107206@users.noreply.github.com>
2023-10-31 15:25:53 -0400

    Modified: idk

--- script/minesweeper.js
+++ script/minesweeper.js
@@ -26,13 +26,55 @@ const initializeGame = (gameData) => {
     return isMine;
   };
 
+  const countSurroundingMines = (currentX, currentY) => {
+    let surroundingMines = 0;
+    for (let dx = -1; dx < 2; dx++) {
+      for (let dy = -1; dy < 2; dy++) {
+        let x = currentX + dx;
+        let y = currentY + dy;
+        coordinateListOfMines.map(i => {
+          if (i[0] == x && i[1] == y) {
+            surroundingMines++;
+          }
+        });
+      }
+    }
+    return surroundingMines;
+  };
+
+  const searchForSurroundingEmptyMines = (selectedX, selectedY) => {
+    for (let currentX = -1; currentX < 2; currentX++) {
+      for (let currentY = -1; currentY < 2; currentY++) {
+        let targetX = selectedX + currentX;
+        let targetY = selectedY + currentY;
+        let conditions = [
+          targetX >= 0,
+          targetX < gameData.columns,
+          targetY >= 0,
+          targetY < gameData.rows
+        ];
+
+        if (conditions.every(x => x == 1)) {
+          console.log(targetX, targetY);
+          let surroundingMines = countSurroundingMines(targetX, targetY);
+  
+          if (surroundingMines == 0) {
+            let tile = document.querySelector(`#tile_${targetX}_${targetY}`);
+            tile.textContent = surroundingMines;
+            searchForSurroundingEmptyMines(currentX, currentY);
+          }
+        }
+      }
+    }
+  }
+
   const terminateGame = async () => {
     await hideElement('#gameScreen', 1);
     await showElement('#mainScreen', 0);
   };
   
   document.querySelector('#gameBoard').innerHTML = '';
-  let healthPoint, countOfMine = gameData.mines;
+  let healthPoint, countOfMine = gameData.mines, countOfFlag = gameData.columns * gameData.rows - gameData.mines;
   
   if (gameData.difficulty == 0) {
     healthPoint = 4;
@@ -47,7 +89,8 @@ const initializeGame = (gameData) => {
     document.querySelector('#difficulty').textContent = '餓「死」';
   }
   document.querySelector('#remainingHealth').textContent = '❤️'.repeat(healthPoint);
-  document.querySelector('#remainingMine').textContent = countOfMine;
+  document.querySelector('#remainingMines').textContent = countOfMine;
+  document.querySelector('#remainingFlags').textContent = countOfFlag;
   
   ['#mode_flag','#mode_mine'].map((x, i, a) => {
     if (i == clickMode) {
@@ -64,58 +107,51 @@ const initializeGame = (gameData) => {
   });
   
   let coordinateListOfMines = generateMines(countOfMine);
-  for (let i = 0; i < gameData.columns * gameData.rows; i++) {
-    let tile = document.createElement('span');
-    tile.textContent = 'ㅤ';
-    document.querySelector('#gameBoard').append(tile);
-    
-    let currentX = Math.floor(i / 4);
-    let currentY = i % 4;
-    
-    tile.onclick = () => {
-      if (clickMode == 0) {
-        if (!tile.classList.contains('flag')) {
-          tile.classList.add('flag');
-          let isMine = checkMine(coordinateListOfMines, currentX, currentY);
-          if (isMine) {
-            countOfMine--;
-          }
-        } else {
-          tile.classList.remove('flag');
-          let isMine = checkMine(coordinateListOfMines, currentX, currentY);
-          if (isMine) {
-            countOfMine++;
-          }
-        }
-      } else {
-        let isMine = checkMine(coordinateListOfMines, currentX, currentY);
+  console.log(coordinateListOfMines);
+  for (let currentX = 0; currentX < gameData.rows; currentX++) {
+    for (let currentY = 0; currentY < gameData.columns; currentY++) {
+      let tile = document.createElement('span');
+      tile.id = `tile_${currentX}_${currentY}`;
+      tile.textContent = 'ㅤ';
+      document.querySelector('#gameBoard').append(tile);
 
-        if (isMine && healthPoint == 0 && countOfMine == 0) {
-          terminateGame();
-        } else if (isMine) {
-          healthPoint--;
-          countOfMine--;
-          
-          document.querySelector('#remainingHealth').textContent = '❤️'.repeat(healthPoint);
-          document.querySelector('#remainingMine').textContent = countOfMine;
-          
-          tile.classList.add('boom');
-        } else {
-          let surroundingMines = 0;
-          for (let dx = -1; dx < 2; dx++) {
-            for (let dy = -1; dy < 2; dy++) {
-              let x = currentX + dx;
-              let y = currentY + dy;
-              coordinateListOfMines.map(i => {
-                if (i[0] == x && i[1] == y) {
-                  surroundingMines++;
-                }
-              });
+      tile.onclick = () => {
+        if (!tile.classList.contains('boom')) {
+          if (clickMode == 0) {
+            if (!tile.classList.contains('flag')) {
+              if (countOfFlag > 0) {
+                tile.classList.add('flag');
+                countOfFlag--;
+              }
+            } else {
+              tile.classList.remove('flag');
+              countOfFlag++;
+            }
+            document.querySelector('#remainingFlags').textContent = countOfFlag;
+          } else {
+            let isMine = checkMine(coordinateListOfMines, currentX, currentY);
+            
+            if (isMine && (healthPoint == 0 || countOfMine == 0)) {
+              terminateGame();
+            } else if (isMine) {
+              healthPoint--;
+              countOfMine--;
+              
+              document.querySelector('#remainingHealth').textContent = '❤️'.repeat(healthPoint);
+              document.querySelector('#remainingMines').textContent = countOfMine;
+              
+              tile.classList.add('boom');
+            } else {
+              let surroundingMines = countSurroundingMines(currentX, currentY);
+              tile.textContent = surroundingMines;
+
+              if (surroundingMines == 0) {
+                //searchForSurroundingEmptyMines(currentX, currentY);
+              }
             }
           }
-          tile.textContent = surroundingMines;
         }
       }
-    };
+    }
   }
 }
